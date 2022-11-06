e1b8b97976d603feee66a09076ceb930df76d8dc
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2022-11-06 13:42:42 +0200

    [Refactor] Jest + Generate React CLI configuration
    Configured Jest for testing and generate-react-cli for component generation.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,23 @@ const nextConfig = {
   },
   eslint: {
     dirs: ['app', 'components', 'lib', 'src']
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'tailwindui.com',
+        port: '',
+        pathname: '/**'
+      }
+    ]
   }
 }
 
