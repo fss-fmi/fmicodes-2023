564116c4de0f5fee850887da891b36d1a649530f
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-02-21 00:46:37 +0200

    [Refactor] Changed image field thumbnail
    Changed the thumbnail to use an image from the heroicons library.

--- apps/fmi-codes-site/components/form-image-field/form-image-field.tsx
+++ apps/fmi-codes-site/components/form-image-field/form-image-field.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+import { DocumentPlusIcon } from '@heroicons/react/24/outline';
 
 export interface FormImageFieldProps {
   name: string;
@@ -24,21 +25,7 @@ export function FormImageField(props: FormImageFieldProps) {
         )}
         <div className="flex flex-col items-center justify-center w-full h-36 my-2 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg
-              aria-hidden="true"
-              className="w-10 h-10 mb-3 text-gray-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-              ></path>
-            </svg>
+            <DocumentPlusIcon className="w-10 h-10 mb-3 text-gray-400" />
             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
               <span className="font-semibold">
                 Кликнете тук за да качите снимка
