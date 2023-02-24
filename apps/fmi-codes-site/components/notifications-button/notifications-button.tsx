dfb9c90201061379e6f09bdad653dca6386eab13
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-02-24 18:47:08 +0200

    [Refactor] Modal is now in a separate component
    Refactored the popup modal to be a separate component, as it will be used in other places as well.

--- apps/fmi-codes-site/components/notifications-button/notifications-button.tsx
+++ apps/fmi-codes-site/components/notifications-button/notifications-button.tsx
@@ -1,58 +1,29 @@
 'use client';
+
 import FancyButton from '../fancy-button/fancy-button';
 import { BellIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
+import FancyModal from '../fancy-modal/fancy-modal';
 
 export function NotificationsButton() {
-  const [showModal, setShowModal] = useState(false);
+  const [isModalShown, setIsModalShown] = useState(false);
 
   return (
     <>
       <FancyButton
         isPrimary
         onClick={() => {
-          setShowModal(true);
+          setIsModalShown(true);
         }}
       >
         <BellIcon className="block h-4 w-4" />
       </FancyButton>
 
-      {showModal ? (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-800 outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Уведомления</h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 h-72 flex-auto">a</div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
+      <FancyModal
+        isModalShown={isModalShown}
+        setIsModalShown={setIsModalShown}
+        title="Уведомления"
+      ></FancyModal>
     </>
   );
 }
