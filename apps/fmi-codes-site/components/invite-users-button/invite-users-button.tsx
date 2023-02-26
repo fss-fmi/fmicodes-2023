a39a7b0b3e474d38095ac2a1afc738941a10abe4
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-02-26 22:31:33 +0200

    [WIP] Invite users button
    Working on a button for sending team invites to users.

---
+++ apps/fmi-codes-site/components/invite-users-button/invite-users-button.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Technology } from '@prisma/client';
+import FancyButton from '../fancy-button/fancy-button';
+import { useEffect, useState } from 'react';
+import FancyModal from '../fancy-modal/fancy-modal';
+import { UserPlusIcon } from '@heroicons/react/24/outline';
+import FormTextField from '../form-text-field/form-text-field';
+import { useForm } from 'react-hook-form';
+
+export interface InviteUsersButtonProps {
+  technologies: Technology[];
+}
+
+export function InviteUsersButton(props: InviteUsersButtonProps) {
+  const [isModalShown, setIsModalShown] = useState(false);
+
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useForm({ mode: 'onChange' });
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) => {
+      const delayDebounceFn = setTimeout(() => {
+        console.log(value);
+        // Send Axios request here
+      }, 3000);
+
+      return () => clearTimeout(delayDebounceFn);
+    });
+    return () => subscription.unsubscribe();
+  }, [watch]);
+
+  return (
+    <>
+      <FancyButton
+        isPrimary
+        onClick={() => {
+          setIsModalShown(true);
+        }}
+      >
+        <UserPlusIcon className="block h-4 w-4" />
+      </FancyButton>
+
+      <FancyModal
+        isModalShown={isModalShown}
+        setIsModalShown={setIsModalShown}
+        title="Покани потребители"
+      >
+        <FormTextField
+          name="query"
+          label="Търсене"
+          placeholder="Име на участник"
+          register={register}
+          errors={errors}
+        />
+      </FancyModal>
+    </>
+  );
+}
+
+export default InviteUsersButton;
