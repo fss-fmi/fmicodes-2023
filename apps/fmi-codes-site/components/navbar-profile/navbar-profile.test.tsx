89920b5f76644331f67a8bd1d3287b02afc4fd39
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-03-02 02:31:28 +0200

    [Refactor] Removed everything every other component

--- apps/fmi-codes-site/components/navbar-profile/navbar-profile.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NavbarProfile from './navbar-profile';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NavbarProfile />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- apps/fmi-codes-site/components/team-form/team-form.tsx
+++
@@ -1,9 +0,0 @@
-export function TeamForm() {
-  return (
-    <div>
-      <h1>Welcome to TeamForm!</h1>
-    </div>
-  );
-}
-
-export default TeamForm;
