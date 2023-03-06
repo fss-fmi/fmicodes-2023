d3a1601eeb5f86e0517ca1046b8e469f686b40fb
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-03-06 21:26:12 +0200

    [Feat] Added FSS logo to homepage
    Updated the homepage to include the organizers' name. ❤️

--- apps/fmi-codes-site/app/page.tsx
+++ apps/fmi-codes-site/app/page.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import HeroLogo from '../components/hero-logo/hero-logo';
 import Countdown from '../components/countdown/countdown';
+import Image from 'next/image';
 
 /**
  * Defines the home page.
@@ -11,9 +12,20 @@ export default function HomePage(): ReactNode {
   return (
     <div className="h-screen">
       <div className="absolute flex top-0 left-0 w-full h-full items-end justify-center">
-        <div className="flex flex-col gap-2 z-10 text-red-600 mb-12">
-          <span className="w-full text-center">ДО НАЧАЛОТО:</span>
-          <Countdown targetDate={'2023/03/17 17:00:00'} />
+        <div className="flex flex-row gap-12 z-10 text-red-600 mb-8">
+          <div className="flex flex-col gap-2">
+            <span className="w-full text-center">ДО НАЧАЛОТО:</span>
+            <Countdown targetDate={'2023/03/17 17:00:00'} />
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="w-full text-center">ОРГАНИЗИРАНО С ❤️ OT:</span>
+            <Image
+              src="/images/fss-logo.png"
+              alt="Лого на ФСС"
+              width={180}
+              height={180}
+            />
+          </div>
         </div>
       </div>
       <HeroLogo />
