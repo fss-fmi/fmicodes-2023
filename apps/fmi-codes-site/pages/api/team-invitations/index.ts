c0f4e7d1b8ca366cb4fa88822024d51c4a8cee2b
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-03-07 12:36:36 +0200

    [Feat] POST /api/team-invitations endpoint
    Added an API endpoint for sending invitations to teams.

--- apps/fmi-codes-site/pages/api/team-invitations/index.ts
+++ apps/fmi-codes-site/pages/api/team-invitations/index.ts
@@ -5,6 +5,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '../auth/[...nextauth]';
 import { getUserBySession } from '../users/self';
 import prisma from '../../../lib/prismadb';
+import { UserWithoutPasswordWithJoins } from '../../../lib/types';
 
 const handler = nextConnect(onError);
 
@@ -36,3 +37,62 @@ export async function getInvitations(userId: string) {
     },
   });
 }
+
+// POST /api/team-invitations
+handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+
+  const inviter = await getUserBySession(session);
+  if (!inviter) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+
+  const invitations = createInvitation(inviter, req.body.inviteeId);
+  res.status(200).json(invitations);
+});
+
+export async function createInvitation(
+  inviter: UserWithoutPasswordWithJoins,
+  inviteeId: string
+) {
+  if (inviter.id === inviteeId) {
+    throw new Error('Нямате право да поканите сам себе си.');
+  }
+
+  if (!inviter.teamCaptain) {
+    throw new Error('Само капитаните могат да поканят играчи.');
+  }
+
+  const invitee = await prisma.user.findUnique({
+    where: {
+      id: inviteeId,
+    },
+  });
+
+  if (!invitee) {
+    throw new Error('Няма такъв играч.');
+  }
+
+  if (invitee.teamId) {
+    throw new Error('Играчът вече е в отбор.');
+  }
+
+  const team = inviter.teamCaptain;
+
+  return await prisma.teamInvitation.create({
+    data: {
+      teamId: team.id,
+      userId: invitee.id,
+    },
+    include: {
+      team: true,
+    },
+  });
+}
+
+export default handler;
