8d9167daaeb4bbfb3da6fbb60fb20c5de60b007e
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-03-17 01:27:17 +0200

    [Feat] Add missing roles on bot restart
    Missing team roles and channels are now added on server start.

--- apps/fmi-codes-discord-bot/src/app/bot/bot.gateway.ts
+++ apps/fmi-codes-discord-bot/src/app/bot/bot.gateway.ts
@@ -4,6 +4,7 @@ import { Client, ColorResolvable } from 'discord.js';
 import environment from '../environments/environment';
 import { PrismaService } from '../database/prisma.service';
 import { HandleLogging } from '../logger/logger.handler';
+import { TeamService } from '../team/team.service';
 
 @Injectable()
 export class BotGateway {
@@ -12,7 +13,8 @@ export class BotGateway {
   constructor(
     @InjectDiscordClient()
     private readonly client: Client,
-    private readonly prisma: PrismaService
+    private readonly prisma: PrismaService,
+    private readonly teamService: TeamService
   ) {}
 
   @Once('ready')
@@ -36,6 +38,29 @@ export class BotGateway {
           name: technology.name,
           color: technology.color as ColorResolvable,
         });
+      }
+    }
+
+    // Create team roles if they don't exist
+    this.logger.log('Creating team roles and channels...');
+    const teams = await this.prisma.team.findMany();
+
+    const roles = await guild.roles.fetch();
+    const channels = await guild.channels.fetch();
+    for (const team of teams) {
+      let role = roles.find((role) => role.name.includes(team.name));
+      if (!role) {
+        this.logger.log(`Creating role ${team.name}`);
+        role = await this.teamService.createTeamRole(guild, team);
+      }
+
+      const channel = channels.find(
+        (channel) => channel.name === `✨ ${team.name} ✨`
+      );
+
+      if (!channel) {
+        this.logger.log(`Creating channel ${team.name}`);
+        await this.teamService.createTeamChannels(guild, team, role);
       }
     }
   }
