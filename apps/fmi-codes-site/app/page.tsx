fc16de3d77e612380c6bc8dad765c5eb4eb81112
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-03-18 01:34:15 +0200

    [Feat] Refactor time until the start to time to end
    Changed timer to display the time until the presentations.

--- apps/fmi-codes-site/app/page.tsx
+++ apps/fmi-codes-site/app/page.tsx
@@ -30,8 +30,8 @@ export default async function HomePage(): Promise<ReactNode> {
         <div className="absolute flex top-0 left-0 w-full h-full items-end justify-center">
           <div className="flex flex-row gap-12 z-10 text-red-600 mb-8">
             <div className="flex flex-col gap-2">
-              <span className="w-full text-center">ДО НАЧАЛОТО:</span>
-              <Countdown targetDate={'2023/03/17 17:00:00'} />
+              <span className="w-full text-center">ДО КРАЯ:</span>
+              <Countdown targetDate={'2023/03/19 16:00:00'} />
             </div>
             <div className="flex flex-col gap-2">
               <span className="w-full text-center">ОРГАНИЗИРАНО С ❤️ OT:</span>
